1e462e84d2a81e7c0d1ce9ffb1cfad9494afdcca
black-trooper <hermanmiller.dkr@gmail.com>
2018-09-21 00:28:18 +0900

    Support karma-coverage-istanbul-reporter

--- test/tests.Spec.js
+++ test/tests.Spec.js
@@ -60,10 +60,19 @@ describe('Given the KarmaCoveralls Module', function () {
 
   it('should throw an exception if "coveralls" does not precede "coverage" in the reporters list', function (done) {
     expect(CoverallsReporter.bind(this, {}, {}, logger))
-      .to.throw(Error, /coverage reporter should precede coveralls/);
+      .to.throw(Error, /coverage or coverage-istanbul reporter should precede coveralls/);
+    expect(CoverallsReporter.bind(this, mockConfig, {}, logger))
+      .to.throw(Error, /coverage or coverage-istanbul reporter should precede coveralls/);
       done();
   });
 
+
+  it('should throw an exception if "coveralls" does not precede "coverage-istanbul" in the reporters list', function (done) {
+    mockConfig.reporters = ['coveralls','coverage-istanbul']
+    expect(CoverallsReporter.bind(this, mockConfig, {}, logger))
+      .to.throw(Error, /coverage or coverage-istanbul reporter should precede coveralls/);
+    done();
+  });
 
   describe('when given the right parameters', function () {
     beforeEach(function () {
@@ -145,6 +154,79 @@ describe('Given the KarmaCoveralls Module', function () {
       mockConfig.coverageReporter = {
         type: 'lcov',
         dir: 'dummy'
+      };
+
+      var result = new CoverallsReporter(mockConfig, helper, logger);
+      result._onExit(function () {
+        expect(coverallsMock.sendToCoveralls).to.not.have.been.called;
+        done();
+      });
+    });
+  });
+
+  describe('when given the right parameters for coverage-istanbul', function () {
+    beforeEach(function () {
+
+      helper = {
+        isDefined: function () {
+          return true;
+        }
+      };
+
+      logger = {
+        create: function () {
+          return {
+            debug: function () {
+            },
+            info: function () {
+            }
+          }
+        }
+      };
+
+      mockConfig = {
+        basePath: '',
+        autoWatch: false,
+        reporters: ['coverage-istanbul', 'coveralls']
+      };
+    });
+
+
+    it('should allow using coverageIstanbulReporter.dir', function (done) {
+      mockConfig.coverageIstanbulReporter = {
+        dir: dir,
+        reporters: ['lcov']
+      };
+
+      var result = new CoverallsReporter(mockConfig, helper, logger);
+      result._onExit(function () {
+
+        expect(coverallsMock.sendToCoveralls).to.have.been.called;
+        done();
+      });
+    });
+
+
+    it('should correctly merge coverage data', function (done) {
+      mockConfig.coverageIstanbulReporter = {
+        dir: dir,
+        reporters: ['lcov']
+      };
+
+      var result = new CoverallsReporter(mockConfig, helper, logger);
+      result._onExit(function () {
+        var expected = fs.readFileSync('test/expected/lcov.info');
+        expect(coverallsMock.convertLcovToCoveralls)
+          .to.have.been.calledWith(expected.toString());
+        done();
+      });
+    });
+
+
+    it('should not send missing coverage data', function (done) {
+      mockConfig.coverageIstanbulReporter = {
+        dir: 'dummy',
+        reporters: ['lcov']
       };
 
       var result = new CoverallsReporter(mockConfig, helper, logger);
